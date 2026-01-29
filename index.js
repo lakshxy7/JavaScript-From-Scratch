bc51a9d52aa33331676f3a1aef77755ab705c6c8
lakshyx7 <lakshaydeepm25@gmail.com>
2026-01-29 13:53:34 +0530

    setting text content

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log(`this is cool`);
-console.log(`this is frk cool`);
-window.alert(`this is an alert`)
-window.alert(`this is an alert`)
+// console.log(`this is cool`);
+// console.log(`this is frk cool`);
+// window.alert(`this is an alert`)
+// window.alert(`this is an alert`)
+
+document.getElementById("myh1").textContent=`Hello Again`;
+document.getElementById("myp").textContent=`setting paragraph now`;
