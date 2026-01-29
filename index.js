14f6945c743a984fe0a7d4f396a810ba885eba9c
lakshyx7 <lakshaydeepm25@gmail.com>
2026-01-29 15:52:38 +0530

    arithmetic operators

--- index.js
+++ index.js
@@ -4,9 +4,18 @@
 // window.alert(`this is an alert`)
 
 let fullName="Lakshaydeep";
-let age= 25;
+let students= 25;
+// students=students+1;
+// students=students*5;
+// students=students/5;
+// students+=1;
+// students-=1;  these are called augmented assignment operator
+// students++
+// students--; these are increment operators
+// operator precedence BODMAS
 let student = false;
 
+
 document.getElementById("p1").textContent= `Your name is ${fullName}`;
-document.getElementById("p2").textContent=`Your age is ${age}`;
+document.getElementById("p2").textContent=`number of students are ${students}`;
 document.getElementById("p3").textContent=`are you a student?${student}`
