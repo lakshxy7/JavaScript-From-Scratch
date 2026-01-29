81bfd050750e858a8ccca6bfc58fc99a71e0917f
lakshyx7 <lakshaydeepm25@gmail.com>
2026-01-29 16:05:43 +0530

    Text Input using windows prompt-1

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-// console.log(`this is cool`);
-// console.log(`this is frk cool`);
-// window.alert(`this is an alert`)
-// window.alert(`this is an alert`)
+// User Input using windows prompt
 
-let fullName="Lakshaydeep";
-let students= 25;
-// students=students+1;
-// students=students*5;
-// students=students/5;
-// students+=1;
-// students-=1;  these are called augmented assignment operator
-// students++
-// students--; these are increment operators
-// operator precedence BODMAS
-let student = false;
+let username;
+username =window.prompt("whats your username?");
+
+document.getElementById("p1").textContent=` the user name is ${username}`;
+
+let age= window.prompt("enter your age");
 
 
-document.getElementById("p1").textContent= `Your name is ${fullName}`;
-document.getElementById("p2").textContent=`number of students are ${students}`;
-document.getElementById("p3").textContent=`are you a student?${student}`
+age=age*45;
+document.getElementById("p2").textContent=`Boom your new age is ${age}`;
