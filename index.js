f91b39115fb52d8562f519246163ec44b53b6fa5
lakshyx7 <lakshaydeepm25@gmail.com>
2026-02-06 22:01:35 +0530

    maths functions

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
+//Math = built in object that provides a collection of properties and methods
+let x=3.14 
+let y=2
+let a=4 
+let b=5
 
- let count=0
 
-const counter = document.getElementById("counter");
-const increasebt=document.getElementById("inc")
-const decreasebt=document.getElementById("dec")
-const reset=document.getElementById("reset")
-counter.textContent=count;
+// z=Math.round(x)
+// z=Math.floor(x)
+// z=Math.ceil(x)
+// z=Math.trunc(x)
+// z=Math.pow(x,y)
+// z=Math.sqrt(x)
+// z=Math.log(10)
+// z=Math.tan(45);
+// z=Math.abs(x)  gives positive number
+// z=Math.sign(x) +/-/0
+// let z=Math.max(x,y);  for maximum 
+ 
+let z=Math.min(a,b,y); 
 
-increasebt.onclick=function(){
-  count = count + 1;
- counter.textContent = count;  
+console.log(z)
 
-};
 
-decreasebt.onclick=function(){
-  count = count -1;
- counter.textContent = count;  
-
-}
-
-reset.onclick=function(){
- count=0;
-counter.textContent=count;
-
-}
