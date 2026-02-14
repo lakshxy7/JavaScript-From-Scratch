0a5222dea813a8fa1e47c0bbcc00a12e2f766845
lakshyx7 <lakshaydeepm25@gmail.com>
2026-02-14 21:37:37 +0530

    ternary operator

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-const subscribebtn= document.getElementById("cbox1");
-const visaBtn= document.getElementById("visa");
-const mastercardBtn= document.getElementById("mastercard");
-const paypalBtn= document.getElementById("paypal");
-const mysubmit= document.getElementById("mysubmit");
-const subResult= document.getElementById("subResult");
-const payResult= document.getElementById("payResult");
+//ternary operator = a shortcut to if and else statements
+//                  helps to assign a variable baed on a condition
+//              condition? codeIfTrue : codeIfFalse
 
 
+// let age= 7;
+// message=age>=18 ?"youre an adult":"welcome to the island"
+// window.alert(message)
 
-mysubmit.onclick= function(){
+// let time=16;
+// let greeting= time<12?"morning":"evening";
+// window.alert(greeting)
 
-if(subscribebtn.checked){
-    subResult.textContent=`you are subscribed`;
-
-}
-else
-{   subResult.textContent=`you are not subscribed`;  }
-
-if(visaBtn.checked){
-    payResult.textContent=`you'll pay using visa?`;
-
-}
-else if(mastercardBtn.checked){
- payResult.textContent=`you'll pay using master card?`;
-}
-else if(paypalBtn.checked){
- payResult.textContent=`you'll pay using paypal?`;
-}
-else{
-
-   payResult.textContent=`please select a payment method`;  
-}
-
-
-
-
-}
-
-
-
-//<input type="radio"  id="paypal"  name="card">
-//<label for="paypal">Paypal</label>  <br> <br>
-// with using for we can allow selection of radio button by clicking on text box
-//checked is used in radio and checkboxes to check if they are checked or not it gives a boolean output
+let isStudent =false;
+let message =isStudent ? "you are a student": "not student"
+window.alert(message)
