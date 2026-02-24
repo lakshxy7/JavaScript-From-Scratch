abfb315fa896d02852ea171e83eb574af4e690df
lakshyx7 <lakshaydeepm25@gmail.com>
2026-02-24 18:07:32 +0530

    functions in JS-2

--- index.js
+++ index.js
@@ -6,22 +6,50 @@
 // The values we pass into a function when calling it are called arguments.
 // The variables written inside the function definition are called parameters.
 
-function happyBirthday(username, age){
-console.log(`Happ Birthday1 ${username}`);
-console.log(`Happ Birthday0`);
-console.log(`Happ Birthday2`);
-console.log(`Happ Birthday3`);
-console.log(`Happ Birthday4`);
-console.log(`you are ${age} years old`);
-}
 
 // When we call the function, we send values to it.
 // These values ("Brocode" and 25) are arguments.
-happyBirthday("Brocode", 25);
+
 
 // The order of parameters matters.
 // The first value goes to username.
 // The second value goes to age.
 // If you change the order, the output will change.
 
-happyBirthday("SB", 100);
+
+
+function add(x,y){
+
+// x and y are parameters
+// This function returns x raised to the power of y
+// ** means exponent (power)
+
+return x**y;
+}
+
+// 4 and 6 are arguments
+// So this becomes 4 raised to the power 6 (4^6)
+let answer = add(4,6);
+
+console.log(answer); // prints 4096
+
+
+
+function isValidEmail(email){
+
+  // The includes() method checks if a string contains a specific value
+  // Here we are checking if the email contains "@" and "."
+  
+  if (email.includes("@","."))
+  {
+    return true;   // If condition is true, function returns true
+  }
+  else
+  {
+    return false;  // Otherwise it returns false
+  }
+
+}
+
+// This will check if the string contains "@" and "."
+console.log(isValidEmail("laks.@"));
