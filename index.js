aad61f39769f3128dbf951ce038dcce129912690
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-03 12:34:57 +0530

    forEach()-1

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
-// callback = a function that is passed as an argument
-// to another function
+//forEach()= method used to iterate over the elements of an array
+// and apply a specified function (callback ) to each element
+// array.forEach(callback)
+// element aregument are provided, 
+//element index and array are provided
+let numbers =[1,2,3,4,5];
 
-// used to handle asynchronous operations:
-// 1. Reading a file
-// 2. Network requests
-// 3. Interacting with databases
+numbers.forEach(square);
+numbers.forEach(display);
 
-// Think of it like:
-// "Hey, when you're done, run this function next."
-
-// ---------------------------------------------
-
-// We are calling sum()
-// - displayPage is the callback function
-// - 1 and 2 are the numbers to add
-
-sum(displayPage, 1, 2);
-
-function sum(callback, x, y){
-
-    // Add the two numbers
-    let result = x + y;
-
-    // Once calculation is finished,
-    // call the callback function
-    // and send the result to it
-    callback(result);
+function display(element){
+    console.log(element);
 }
 
-// ---------------------------------------------
 
-function displayConsole(result){
 
-    // This function receives the result
-    // and prints it in the console
-    console.log(result);
+function double(element,index,array){
+    array[index]=element*2;
+
 }
 
-// ---------------------------------------------
+function triple(element,index,array){
+    array[index]=element*3;
+}
 
-function displayPage(result){
+function square(element,index,array){
+    array[index]=Math.pow(element,2)
+}
 
-    // This function receives the result
-    // and displays it inside an HTML element
-    // with id "myH1"
-
-    document.getElementById("myH1").textContent = result;
+function cube(element,index,array){
+    array[index]=Math.pow(element,3)
 }
