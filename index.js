c76a0a1ff6da473436a83c006b171604dc7368ed
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-08 23:18:28 +0530

    map function-2

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 // .map()=accepts a callback and applies that function
 //to each element of an array, then return a new array
 
-const numbers=[1,2,3,4,5];
+const dates=["2024-1-10","2025-2-20","2026-3-30"];
 
-const squares=numbers.map(square)
-const cubes=numbers.map(cube);
-console.log(cubes)
+const formattedDates=dates.map(formatDates);
 
 
 
-console.log(squares);
-function square(element){
+function formatDates(element){
 
 
-    return Math.pow(element,2)
+const parts=element.split("-");
+return`${parts[2]}/${parts[1]}/${parts[0]}` ;
 }
 
-function cube(element){
-
-
-
-    return Math.pow(element,3);
-}
-
-const students=["squidward","patrick","spongebob","sandy"]
-const studentsUpper=students.map(upperCase);
-const studentsLower=students.map(lowercase);
-
-console.log(studentsUpper);
-console.log(studentsLower);
-function upperCase(element){
-
-
-    return element.toUpperCase();
-}
-function lowercase(element){
-
-
-    return element.toLowerCase();
-}
+console.log(formattedDates)
