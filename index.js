3addda6585f1201a33e9d201b6b2d8a108acb63c
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-08 23:10:17 +0530

    map() function-1

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-//forEach()= method used to iterate over the elements of an array
-// and apply a specified function (callback ) to each element
-// array.forEach(callback)
-// element aregument are provided, 
-//element index and array are provided
-let fruits =["APPLE","orange","BANANA","coconut"];
+// .map()=accepts a callback and applies that function
+//to each element of an array, then return a new array
+
+const numbers=[1,2,3,4,5];
+
+const squares=numbers.map(square)
+const cubes=numbers.map(cube);
+console.log(cubes)
 
 
-fruits.forEach(capitalize);
+
+console.log(squares);
+function square(element){
 
 
-fruits.forEach(display);
-
-function toUpperCase(element,index,array){
-    
-array[index]=element.toUpperCase();
-
+    return Math.pow(element,2)
 }
 
+function cube(element){
 
 
-function display(element){
 
-
-    console.log(element);
+    return Math.pow(element,3);
 }
 
+const students=["squidward","patrick","spongebob","sandy"]
+const studentsUpper=students.map(upperCase);
+const studentsLower=students.map(lowercase);
 
-function toLowerCase(element,index,array){
-    
-array[index]=element.toLowerCase();
+console.log(studentsUpper);
+console.log(studentsLower);
+function upperCase(element){
 
+
+    return element.toUpperCase();
 }
+function lowercase(element){
 
-function capitalize(element,index,array){
-    
-array[index]=element.charAt(0).toLowerCase()+ element.slice(1);
 
+    return element.toLowerCase();
 }
-
