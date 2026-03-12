dddff7e9e7143b3a6e7b458792ed7d3f677c0e98
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-12 14:00:55 +0530

    arrow functions -1

--- index.js
+++ index.js
@@ -1,66 +1,36 @@
-// FUNCTION DECLARATION
-// A function declaration is a named block of reusable code
-// that performs a specific task when it is called.
-// It is created using the "function" keyword followed by
-// the function name and parentheses ().
+// ARROW FUNCTION
+// Arrow functions are a shorter and cleaner way to write
+// function expressions in JavaScript.
+// They are mostly used for small or simple functions.
+// The syntax uses an arrow => instead of the "function" keyword.
 
-function greet(name) {
-    console.log("Hello " + name);
+// syntax
+// (parameters) => { code }
+
+// normal function
+const hello=function hello(){
+    console.log("hello");
 }
 
-greet("Lakshay"); // calling the function
+hello(); // calling the function
+
+// arrow function
+const tello = (name) => console.log("tello",name);
+
+tello("lakshay"); // calling the function
 
 
 
-// FUNCTION EXPRESSION ***
-// A function expression is a function that is stored inside
-// a variable. The function acts like a value assigned to that variable.
-// The variable name is used to call the function.
-//simply way to defined functions as variable
-const greetUser = function(name) {
-    console.log("Hello " + name);
-};
-
-greetUser("Lakshay"); // calling the function
 
 
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
- const  numbers=[1,2,3,4,5,6];
- const squares= numbers.map( function (element){
+//zzzzzzzzzzzzzzz
 
-    return Math.pow(element,2);
- }
-);
- console.log(squares)
+setTimeout(function(){
 
- 
+    console.log("hello3");
+},1500)
 
+setTimeout(()=>{
 
- const cubes= numbers.map( function (element){
-
-    return Math.pow(element,3);
- }
-);
- console.log(cubes)
-
-
- const evenNums= numbers.filter(function(element){
-
-    return element%2===0;
- })
- console.log(evenNums)
-
- const oddNums= numbers.filter(function(element){
-
-    return element%2!==0;
- })
- console.log(oddNums)
-
-
-
- const total= numbers.reduce(function(accumulator,element){
-
-return accumulator+element
-
- })
-  console.log(total)
+    console.log("hello4");
+},2000)
