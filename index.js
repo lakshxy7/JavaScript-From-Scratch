11f7c64d98882402f27653c4d512c7693b24a126
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-12 13:24:54 +0530

    function expression -1

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-// reduce() in JavaScript
+// function declaration = define a resuable block of 
+// code that performs a specific task
 
-// reduce() is used to take all elements of 
-// an array and reduce them to a single value.
+//function expressions=a way to define functions as 
+ //values  or variables
+function hello(){
 
-
-// const prices= [5,30,10,25,15,20];
-
-
-// const total=prices.reduce(sum);
-// console.log(total);
-// function sum(accumalator, element){
-
-
-// return accumalator+element;
-// }
-
-const grades=[75,90,80,65,95];
-
-
-const maximum= grades.reduce(getMax);
-console.log(maximum);
-
-function getMax(accumalator,element){
-
-    return Math.max(accumalator, element);
+    console.log("hello");
 }
+hello();
 
-const minimum= grades.reduce(getMin);
-console.log(minimum);
 
-function getMin(accumalator,element){
 
-    return Math.min(accumalator, element);
-}
+  const tello=function(){
+    console.log("tello");
+ }
+ tello();
+
+setTimeout(tello,3000);
+
+setTimeout(function(){console.log("hello")},3000
+
+)
