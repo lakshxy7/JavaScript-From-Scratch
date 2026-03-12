452948cad20c5dfd3211519ea007d42986721332
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-12 13:44:53 +0530

    function expression-2

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
-// function declaration = define a resuable block of 
-// code that performs a specific task
+// FUNCTION DECLARATION
+// A function declaration is a named block of reusable code
+// that performs a specific task when it is called.
+// It is created using the "function" keyword followed by
+// the function name and parentheses ().
 
-//function expressions=a way to define functions as 
- //values  or variables
-function hello(){
-
-    console.log("hello");
+function greet(name) {
+    console.log("Hello " + name);
 }
-hello();
+
+greet("Lakshay"); // calling the function
 
 
 
-  const tello=function(){
-    console.log("tello");
+// FUNCTION EXPRESSION ***
+// A function expression is a function that is stored inside
+// a variable. The function acts like a value assigned to that variable.
+// The variable name is used to call the function.
+//simply way to defined functions as variable
+const greetUser = function(name) {
+    console.log("Hello " + name);
+};
+
+greetUser("Lakshay"); // calling the function
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
+ const  numbers=[1,2,3,4,5,6];
+ const squares= numbers.map( function (element){
+
+    return Math.pow(element,2);
  }
- tello();
+);
+ console.log(squares)
 
-setTimeout(tello,3000);
+ 
 
-setTimeout(function(){console.log("hello")},3000
 
-)
+ const cubes= numbers.map( function (element){
+
+    return Math.pow(element,3);
+ }
+);
+ console.log(cubes)
+
+
+ const evenNums= numbers.filter(function(element){
+
+    return element%2===0;
+ })
+ console.log(evenNums)
+
+ const oddNums= numbers.filter(function(element){
+
+    return element%2!==0;
+ })
+ console.log(oddNums)
+
+
+
+ const total= numbers.reduce(function(accumulator,element){
+
+return accumulator+element
+
+ })
+  console.log(total)
