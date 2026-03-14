f4c8b1c892edab7332872ef8f7bdc66d91093bb3
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-14 12:07:41 +0530

    this Keyword in JS

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-// Objects in JS = A collection of related properties and/or methods
-// can represent real world objects (people,product,places)
-// object =(key:value/function);
-
-//methods  is a function that belongs to an object
+//this= this is a keyword,
+//it is a reference to the object where this is used
+//(the object depends on the immediate context)
+//persopn.name=this.name
+// "this" refers to the object that is
+//  calling the function
 
 const person1={
-    firstName:"Spongebob",
-    lastName:"Squarepants",
-    age:30,
-    isEmployed:true,
-    sayHello: ()=>{console.log("say Hello")},
-    eat: ()=>{console.log("im eating")},
+    name:"spongebob",
+    favFood:"hamburgers",
+    sayHello:function(){console.log(`Hi i am ${this.name} and my favfood is ${this.favFood}`)}
 
 }
-
-const person2={
-    firstName:"Patric",
-    lastName:"Star",
-    age:42,
-    isEmployed:false,
-
-
-
-}
-
-console.log(person1.firstName,person1.lastName);
-console.log(person1.age);
-console.log(person1.isEmployed);
-person1.sayHello();
-person1.eat();
-
-console.log(person2.firstName,person2.lastName);
-console.log(person2.age);
-console.log(person2.isEmployed);
+person1.sayHello(); 
