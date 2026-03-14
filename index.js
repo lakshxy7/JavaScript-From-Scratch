d6d5506bd1d3622fed2d7b646cb72d2c19e29d3f
lakshyx7 <lakshaydeepm25@gmail.com>
2026-03-14 22:32:44 +0530

    Static keyword in JS-1

--- index.js
+++ index.js
@@ -1,68 +1,27 @@
-// CLASSES (ES6 Feature)
-// A class is like a blueprint or template used to create objects.
-// It helps organize code in a cleaner and structured way.
+// The static keyword is used to 
+// create methods or properties that 
+// belong to the class itself, not to the
+// objects created from that class.
 
-class Product {
+class MathUtils{
 
-    // CONSTRUCTOR
-    // The constructor runs automatically when we create a new object.
-    // It initializes (sets up) the values for the object.
+    static PI = 3.14159;
 
-    constructor(name, price){
-
-        // "this" refers to the current object being created.
-
-        this.name = name;   // stores the product name
-        this.price = price; // stores the product price
+    static getDiameter(radius){
+        return radius * 2;
     }
 
-
-    // METHOD
-    // A method is simply a function inside a class.
-    // This method prints the product details.
-
-    displayProduct(){
-
-        console.log(`Product: ${this.name}`);
-        console.log(`Price: ${this.price}`);
-
+    static getCircumference(radius){
+        return 2 * this.PI * radius;
     }
 
-
-    // Another METHOD
-    // This method calculates the price after adding tax.
-
-    calculateTotal(salesTax){
-
-        // formula → price + (price × tax)
-        return this.price + (this.price * salesTax);
-
+    static getArea(radius){
+        return this.PI * radius * radius;
     }
 
 }
 
-
-// Tax rate (5%)
-const salesTax = 0.05;
-
-
-// Creating objects using the class
-// "new" creates a new object based on the Product class
-
-const product1 = new Product("Shirt", 200);
-const product2 = new Product("Pants", 300);
-const product3 = new Product("Jacket", 600);
-
-
-// Calling the method to display product information
-
-product1.displayProduct();
-product2.displayProduct();
-product3.displayProduct();
-
-
-// Calling the method to calculate total price including tax
-
-const total = product1.calculateTotal(salesTax);
-
-console.log(`Total price with tax: ${total}`);
+console.log(MathUtils.PI);
+console.log(MathUtils.getDiameter(10));
+console.log(MathUtils.getCircumference(10));
+console.log(MathUtils.getArea(10));
